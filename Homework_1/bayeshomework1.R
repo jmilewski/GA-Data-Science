6a17bc9e82100c46faece5b3b8e753b8af661af5
Jeff Milewski <jeffmilewski@gmail.com>
2013-06-03 18:17:51 -0400

    bayes

---
+++ Homework_1/bayeshomework1.R
@@ -0,0 +1,11 @@
+# Bayes Homework Code
+
+library('tm')
+library('ggplot2')
+library('e1071')
+library('class')
+
+german.data <- read.table('/Users/jeffmilewski/data_science_class_examples-master/homework/homework_1.txt')
+model <- naiveBayes(V25 ~ ., data = german.data)
+pred <- predict(model, german.data[1:1000,])table(pred, german.data$V25)
+
